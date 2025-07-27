13dc7fff049d12814b8b8f5b0ed49564af1d0db4
priyanshuprafful <priyanshuprafful@gmail.com>
2025-07-27 17:19:30 +0530

    updating ./learn-terraform-2.0

--- functions/main.tf
+++ functions/main.tf
@@ -5,3 +5,23 @@ variable "class" {
 output "class" {
   value = upper(var.class)
 }
+
+variable "classes" {
+  default = {
+    devops = {
+      name = "DevOps"
+      topics = ["jenkins", "docker"]
+    }
+    aws = {
+      name = "aws"
+    }
+  }
+}
+
+output "devops_topics" {
+  value = var.classes["devops"]["topics"]
+}
+
+output "aws_topics" {
+  value = lookup(lookup(var.classes, "aws", null), "topics" , "topics not found - this is null value" )
+}
